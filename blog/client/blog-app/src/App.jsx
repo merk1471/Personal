0d503e751bbe88a98247d4260a6ec1e9fb2aa8fa
Mark <mark.kyrilos@gmail.com>
2024-01-17 15:51:25 -0500

    Added header and post elements, modified css files

--- blog/client/blog-app/src/App.jsx
+++ blog/client/blog-app/src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <main>
+        <header>
+          <a href="" className="logo">Blogify </a>
+          <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+
+        <div className="entries">
+
+          <div className="post">
+            <div className="image">
+              <img src="https://i.cbc.ca/1.7085982.1705520220!/cpImage/httpImage/image.jpg_gen/derivatives/16x9_780/raptors-jazz-basketball.jpg" alt="" />
+            </div>
+            <div className='text'>
+              <h2>Raptors trading 2-time All-Star, 2019 NBA Champion Pascal Siakam to Pacers: reports</h2>
+              <p>The Toronto Raptors are reportedly in the final stages of a trade that would see the team offload two-time all-star forward Pascal Siakam, one of the last remaining ties to the team's 2019 NBA championship.</p>          
+            </div>
+          </div>
+
+          <div className="post">
+            <div className="image">
+              <img src="https://i.cbc.ca/1.7085982.1705520220!/cpImage/httpImage/image.jpg_gen/derivatives/16x9_780/raptors-jazz-basketball.jpg" alt="" />
+            </div>
+            <div className='text'>
+              <h2>Raptors trading 2-time All-Star, 2019 NBA Champion Pascal Siakam to Pacers: reports</h2>
+              <p>The Toronto Raptors are reportedly in the final stages of a trade that would see the team offload two-time all-star forward Pascal Siakam, one of the last remaining ties to the team's 2019 NBA championship.</p>          
+            </div>
+          </div>
+
+          <div className="post">
+            <div className="image">
+              <img src="https://i.cbc.ca/1.7085982.1705520220!/cpImage/httpImage/image.jpg_gen/derivatives/16x9_780/raptors-jazz-basketball.jpg" alt="" />
+            </div>
+            <div className='text'>
+              <h2>Raptors trading 2-time All-Star, 2019 NBA Champion Pascal Siakam to Pacers: reports</h2>
+              <p>The Toronto Raptors are reportedly in the final stages of a trade that would see the team offload two-time all-star forward Pascal Siakam, one of the last remaining ties to the team's 2019 NBA championship.</p>          
+            </div>
+          </div>
+          
+        </div>
+      </main>
   )
 }
 
